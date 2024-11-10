d7de2defb5e8aac154fe564c7d9651f635769773
unknown <mjiyani@tandemdiabetes.com>
2024-11-10 19:09:47 +0530

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,4 +1,21 @@
 import socket  # noqa: F401
+import threading
+
+
+def handle_client(client_socket):
+    print("Handles communication with a single client.")
+    with client_socket:
+        print("Client connected, ready to respond to multiple PING commands...")
+        while True:
+            # Read data from the client
+            data = client_socket.recv(1024)  # Read up to 1024 bytes
+            if not data:
+                break  # Client disconnected
+
+            print("Received command, sending +PONG response...")
+
+            # Send the hardcoded +PONG\r\n response for each command
+            client_socket.sendall(b"+PONG\r\n")
 
 def main():
     print("Logs from your program will appear here!")
@@ -10,22 +27,9 @@ def main():
             client_socket, _ = server_socket.accept()
             print("Client connected, ready to respond to multiple PING commands...")
 
-            # Keep the client socket open to allow multiple commands
-            try:
-                with client_socket:
-                    while True:
-                        # Read data from the client
-                        data = client_socket.recv(1024)  # Read up to 1024 bytes
-                        if not data:
-                            break  # Client disconnected
-
-                        print("Received command, sending +PONG response...")
-
-                        # Send the hardcoded +PONG\r\n response for each command
-                        client_socket.sendall(b"+PONG\r\n")
-
-            except ConnectionResetError:
-                print("Connection closed by client.")
+            # Create a new thread for each client
+            client_thread=threading.Thread(target=handle_client,args=(client_socket,))
+            client_thread.start()
 
 if __name__ == "__main__":
     main()
