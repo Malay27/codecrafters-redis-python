0b5e9513d281a197aeaf331edf724e894033a238
unknown <mjiyani@tandemdiabetes.com>
2024-11-10 14:48:09 +0530

    Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
 import socket  # noqa: F401
 
-
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-    
-    # Set up the server socket 
+
+    # Set up the server socket
     with socket.create_server(("localhost", 6379)) as server_socket:
         # Continuously accept connections
         while True:
-            client_socket,_=server_socket.accept()
-            with client_socket:
-                print("Client connected, sending +PONG response...")
+            client_socket, _ = server_socket.accept()
+            print("Client connected, ready to respond to multiple PING commands...")
 
-                client_socket.sendall(b"+PONG\r\n")
+            # Keep the client socket open to allow multiple commands
+            try:
+                with client_socket:
+                    while True:
+                        # Read data from the client
+                        data = client_socket.recv(1024)  # Read up to 1024 bytes
+                        if not data:
+                            break  # Client disconnected
 
+                        print("Received command, sending +PONG response...")
+
+                        # Send the hardcoded +PONG\r\n response for each command
+                        client_socket.sendall(b"+PONG\r\n")
+
+            except ConnectionResetError:
+                print("Connection closed by client.")
 
 if __name__ == "__main__":
     main()
